ed26ed3e99bf4bd78ab23b24cf631b1ace1dacd7
abhishek6164 <abhishekpipriye2507@gmail.com>
2024-10-12 09:59:31 +0530

    Changes for deployment

---
+++ my-portfolio/build/assets/react-pdf-DdxdozKZ.js
@@ -0,0 +1,1 @@
+import"./react-CK7KRXmA.js";import"./pdfjs-dist-DUdy_spy.js";
--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -1,13 +1,13 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
-
 export default defineConfig({
+  base: "/Portfolio/my-portfolio/",
   build: {
-    chunkSizeWarningLimit: 1000, // Increase the limit or configure manual chunks if necessary
+    outDir: "build", // Change the output directory to 'build'
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // Example of code-splitting, you can adjust based on your actual imports
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
