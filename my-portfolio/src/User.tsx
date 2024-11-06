49e447932f1fc26dc714da44a57fd947ef24aef8
abhishek6164 <abhishekpipriye2507@gmail.com>
2024-11-06 17:06:30 +0530

    "Removed unnecessary newline and updated image path comments in User.tsx"

--- my-portfolio/src/User.tsx
+++ my-portfolio/src/User.tsx
@@ -21,12 +21,11 @@ const Info = {
           backend systems with front-end development. I’m excited to create
           user-friendly web applications and explore new web technologies.`,
 };
-
 const ProjectInfo = [
   {
     title: "Sanatan Ki Virasat",
     desc: "Sanatan Ki Virasat is a project that delves into the timeless heritage of Sanatan Dharma...",
-    image: "/images/vedas.jpeg", // Path should start with '/'
+    image: "/images/vedas.jpeg", // Use correct path starting with '/'
     technologies: ["HTML", "CSS", "Javascript", "Bootstrap"],
     github: "https://github.com/abhishek6164/sanatan-ki-virasat",
     link: "https://abhishek6164.github.io/sanatan-ki-virasat/",
@@ -34,7 +33,7 @@ const ProjectInfo = [
   {
     title: "Weather Update",
     desc: "WeatherNow is a dynamic and responsive weather update website...",
-    image: "/images/weather.jpeg",
+    image: "/images/weather.jpeg", // Use correct path starting with '/'
     technologies: ["React", "API"],
     github: "https://github.com/abhishek6164/weather-now",
     link: "https://abhishek6164.github.io/weather-now/",
@@ -42,7 +41,7 @@ const ProjectInfo = [
   {
     title: "NextInLine",
     desc: "NextInLine is a dynamic e-commerce platform...",
-    image: "/images/ecommerce.jpeg",
+    image: "/images/ecommerce.jpeg", // Use correct path starting with '/'
     technologies: ["React", "Node.JS", "MongoDB", "Tailwind"],
     github: "https://github.com/abhishek6164/next-inline",
     link: "https://abhishek6164.github.io/next-inline/",
